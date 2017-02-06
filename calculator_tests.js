caea344c3bf097fb3d59e50e68c0414517dab8f4
Matthew R Marino <mm02971@srcs.loc>
2017-02-06 08:34:05 -0800

    random save

--- calculator_tests.js
+++ calculator_tests.js
@@ -3,7 +3,7 @@ QUnit.test( "Add digits test", function( assert ) {
     addDigit('1');
     addDigit('2');
     assert.equal(document.getElementById("screen").value, "12", "Passed - Expected 12");
-});
+;
 
 // Test adding one, then two decimals
 QUnit.test( "Add decimal test", function( assert ) {
