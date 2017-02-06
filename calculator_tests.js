e1b89328262de2f3c942d268be84c95dc542687c
Braulio  Resendiz Cordova <br19777@srcs.loc>
2017-02-06 09:15:11 -0800

    adding comments to all matthew's test

--- calculator_tests.js
+++ calculator_tests.js
@@ -5,6 +5,8 @@ QUnit.test( "Add digits test", function( assert ) {
     assert.equal(document.getElementById("screen").value, "12", "Passed - Expected 12");
     current_input = "0";
 });
+
+
 // Test adding one, then two decimals
 QUnit.test( "Add decimal test", function( assert ) {
     addDecimal();
@@ -13,6 +15,8 @@ QUnit.test( "Add decimal test", function( assert ) {
     assert.equal(document.getElementById("screen").value, "0.2", "Passed - Expected 0.2");
     current_input = "0";
 });
+
+// Test square root cureent input
 QUnit.test( "Square Root test", function( assert ) {
     addDigit('4');
     addDigit('9');
@@ -20,17 +24,24 @@ QUnit.test( "Square Root test", function( assert ) {
     assert.equal(document.getElementById("screen").value, "7", "Passed - Expected 7");
     current_input = "0";
 });
+
+// Test squaring current input
 QUnit.test( "Square test", function( assert ) {
     addDigit('7');
     square(current_input);
     assert.equal(document.getElementById("screen").value, "49", "Passed - Expected 49");
     current_input = "0";
-});QUnit.test( "switch sign test", function( assert ) {
+});
+
+// Test switch sign test
+QUnit.test( "switch sign test", function( assert ) {
     addDigit('7');
     changeSign(current_input);
     assert.equal(document.getElementById("screen").value, "-7", "Passed - Expected -7");
     current_input = "0";
 });
+
+
 // Test input limit into calculator
 QUnit.test( "Add input limit test", function( assert ) {
      addDigit('2');
