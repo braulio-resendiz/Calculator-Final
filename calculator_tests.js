012c255bd486f48067b3795d6b06e8c812cfcac6
Matthew R Marino <mm02971@srcs.loc>
2017-02-06 08:42:10 -0800

    root

--- calculator_tests.js
+++ calculator_tests.js
@@ -3,12 +3,20 @@ QUnit.test( "Add digits test", function( assert ) {
     addDigit('1');
     addDigit('2');
     assert.equal(document.getElementById("screen").value, "12", "Passed - Expected 12");
-;
-
+    current_input = "0";
+});
 // Test adding one, then two decimals
 QUnit.test( "Add decimal test", function( assert ) {
     addDecimal();
     addDigit('2');
     addDecimal();
     assert.equal(document.getElementById("screen").value, "0.2", "Passed - Expected 0.2");
+    current_input = "0";
+});
+QUnit.test( "Square Root test", function( assert ) {
+    addDigit('4');
+    addDigit('9');
+    squareRoot(current_input);
+    assert.equal(document.getElementById("screen").value, "7", "Passed - Expected 7");
+    current_input = "0";
 });
